d52c552af93ff2fef4f18f5cfc2fe43725b78cf7
Drallireeh <kenny.herillard@hotmail.fr>
2021-11-26 11:09:21 +0100

    End of exercice

--- src/animal.class.ts
+++ src/animal.class.ts
@@ -1,3 +1,4 @@
+import IAnimal from './animal.interface';
 import Food from './food/food';
 
 // Créer une class Animal qui implémente l'interface IAnimal
@@ -5,8 +6,22 @@ import Food from './food/food';
 // par défaut, la méthode sleep réduit le poids de l'animal de 10
 // le poids d'un animal ne peut pas être négatif
 
-export default class Animal {
+export default class Animal implements IAnimal {
+  name: string;
+  weight: number;
 
-  // CODER ICI
+  constructor(name: string, weight: number) {
+    this.name = name;
+    this.weight = weight;
+  }
+
+  eat <T extends Food>(food: T): void | string {
+    this.weight += 10;
+  }
+
+  sleep(): void {
+    this.weight -= 10;
+    if (this.weight < 0) this.weight = 0;
+  }
 
 }
--- src/array.ts
+++ src/array.ts
@@ -2,24 +2,45 @@ export default class MyArray {
 
   // Coder une méthode oddNumbers qui prend en paramètre un tableau d'entier, et renvoie un tableau uniquement composé
   // des entiers impairs de ce dernier
-  oddNumbers(arr: number[]) {
-    return undefined
+  oddNumbers(arr: number[]): number[] {
+    let oddArray: number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 !== 0) oddArray.push(arr[i]);
+    }
+    return oddArray;
   }
 
   // Coder une méthode sort qui renvoie le tableau passé en paramètre trié par ordre croissant
-  sort(arr: number[]) {
-    return undefined
+  sort(arr: number[]): number[] {
+    let numArray: number[] = arr;
+    numArray.sort(function (a, b) {
+      return a - b;
+    });
+    return numArray;
   }
 
   // Coder une méthode boringFunction qui renvoie true si les string du tableaux ont toutes la même longueur,
   // ou renvoie "relou ta fonction" sinon
-  boringFunction(arr: string[]) {
-    return undefined
+  boringFunction(arr: string[]): boolean | string {
+    let strLength = arr[0].length;
+    let condition = true;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length !== strLength) condition = false;
+    }
+
+    if (condition) return true
+    else return "relou ta fonction"
   }
 
   // Coder une méthode optionalArray qui renvoie true si les 2 arrays d'entiers passés en paramètres sont égaux, false sinon
   // Faire de même avec un 3e tableau passé en paramètre qui sera optionnel
-  optionalArray(a, b, c?) {
-    return undefined
+  optionalArray(a: number[], b: number[], c?: number[]): boolean {
+    let condition: boolean = false;
+
+    if (a.every((val, index) => val === b[index])) condition = true;
+    if (c) {
+      if (a.every((val, index) => val === b[index]) && a.every((val, index) => val === c[index])) condition = true;
+    }
+    return condition
   }
 }
