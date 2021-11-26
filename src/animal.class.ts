4b9153400356d4fb94562f0f5a3ae6abcffdaf2a
KeligMartin <32711301+KeligMartin@users.noreply.github.com>
2021-11-26 11:55:16 +0100

    Update animal.class.ts

--- src/animal.class.ts
+++ src/animal.class.ts
@@ -1,7 +1,7 @@
 import Food from './food/food';
 import IAnimal from './animal.interface';
 
-// Créer une class Animal qui implémente l'interface IAnimal
+// Créer une class Animal qui implémente l'interface IAnima
 // par défaut, la méthode eat augmente le poids de l'animal de 10
 // par défaut, la méthode sleep réduit le poids de l'animal de 10
 // le poids d'un animal ne peut pas être négatif
