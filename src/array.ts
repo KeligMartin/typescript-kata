b3408bf7f53a5c8e92a664ad8e6e6d96de026020
Florian PIAULT <florian.piault@mail-ecv.fr>
2021-11-26 11:37:05 +0100

    exo fini

--- src/array.ts
+++ src/array.ts
@@ -1,25 +1,32 @@
 export default class MyArray {
-
   // Coder une méthode oddNumbers qui prend en paramètre un tableau d'entier, et renvoie un tableau uniquement composé
   // des entiers impairs de ce dernier
   oddNumbers(arr: number[]) {
-    return undefined
+    return arr.filter((n: number) => n % 2);
   }
 
   // Coder une méthode sort qui renvoie le tableau passé en paramètre trié par ordre croissant
   sort(arr: number[]) {
-    return undefined
+    return arr.sort((a: number, b: number) => a - b);
   }
 
   // Coder une méthode boringFunction qui renvoie true si les string du tableaux ont toutes la même longueur,
   // ou renvoie "relou ta fonction" sinon
-  boringFunction(arr: string[]) {
-    return undefined
+  boringFunction(arr: string[]): boolean | string {
+    const eq: boolean = arr.every((str: string) => arr[0].length === str.length);
+
+    return eq || 'relou ta fonction';
   }
 
   // Coder une méthode optionalArray qui renvoie true si les 2 arrays d'entiers passés en paramètres sont égaux, false sinon
   // Faire de même avec un 3e tableau passé en paramètre qui sera optionnel
-  optionalArray(a, b, c?) {
-    return undefined
+  optionalArray(a: number[], b: number[], c?: number[]): boolean {
+    if (a.length !== b.length) return false;
+
+    const eq: boolean = a.every((n: number, i: number) => {
+      if (c) return n === b[i] && n === c[i];
+      return n === b[i];
+    });
+    return eq;
   }
 }
