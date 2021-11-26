ac875163a5f74fa558ffb19be8f466f203335643
vgimonnet <valentin.gimonnet18@gmail.com>
2021-11-26 13:11:37 +0100

    Valentin Gimonnet

--- src/array.ts
+++ src/array.ts
@@ -2,24 +2,44 @@ export default class MyArray {
 
   // Coder une méthode oddNumbers qui prend en paramètre un tableau d'entier, et renvoie un tableau uniquement composé
   // des entiers impairs de ce dernier
-  oddNumbers(arr: number[]) {
-    return undefined
+  oddNumbers(arr: number[]): number[] {
+    return arr.filter(value => value % 2);
   }
 
   // Coder une méthode sort qui renvoie le tableau passé en paramètre trié par ordre croissant
-  sort(arr: number[]) {
-    return undefined
+  sort(arr: number[]): number[] {
+    return arr.sort((a: number, b: number ): number => a - b);
   }
 
   // Coder une méthode boringFunction qui renvoie true si les string du tableaux ont toutes la même longueur,
   // ou renvoie "relou ta fonction" sinon
-  boringFunction(arr: string[]) {
-    return undefined
+  boringFunction(arr: string[]): boolean | string {
+    if (arr.length > 1) {
+      const firstElementLength: number = arr[0].length;
+      for (let i: number = 0; i < arr.length; i++) {
+        if (arr[i].length !== firstElementLength) {
+          return 'relou ta fonction';
+        }        
+      }
+    }
+
+    return true;
   }
 
   // Coder une méthode optionalArray qui renvoie true si les 2 arrays d'entiers passés en paramètres sont égaux, false sinon
   // Faire de même avec un 3e tableau passé en paramètre qui sera optionnel
-  optionalArray(a, b, c?) {
-    return undefined
+  optionalArray(a: number[], b: number[], c?:number[]): boolean {
+    if (c ? (a.length === b.length && a.length === c.length) : (a.length === b.length)) {
+      for(let i: number = 0; i < a.length; i++) {
+        if (c ? (a[i] !== b[i] || a[i] !== c[i]): (a[i] !== b[i])) {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+
+    return true;
+    // return c ? (a === b && a === c) : (a === b);
   }
 }
--- src/vehicle.ts
+++ src/vehicle.ts
@@ -14,20 +14,31 @@ export default class Vehicle {
   // bep bep si le vehicule est une moto
   // brrrrrrrrrrrrr si le vehicule est un camion
   // skrrrt skrrrt dans tout autre cas
-  noise() {
-    return undefined
+  noise(): string {
+    switch (this.type) {
+      case VehicleType.Car:
+        return 'vrooooom';
+        break;
+      case VehicleType.Moto:
+        return 'bep bep';
+        break;
+      case VehicleType.Truck:
+        return 'brrrrrrrrrrrrr';
+        break;    
+      default:
+        return 'skrrrt skrrrt';
+        break;
+    }
   }
 
   // Coder un callback qui renverra la methode noise précedemment codée
   // mais transformera la valeur de retour en majuscule
-  myCb = () => {
-    return undefined
+  myCb = (): string => {
+    return this.noise().toUpperCase();
   }
 
   // Coder une méthode accelerate qui prend en paramètre un callback et renvoie la valeur de noise() concaténée au callback
-  accelerate(cb: () => string) {
-    return undefined
+  accelerate(cb: () => string): string {
+    return this.noise() + cb();
   }
-
-
 }
