0e528bf2ee093a6de16512e1f39eae126fd10c34
vgimonnet <valentin.gimonnet18@gmail.com>
2021-11-26 11:38:24 +0100

    Tp kata : Valentin Gimonnet

--- src/hippopotamus.ts
+++ src/hippopotamus.ts
@@ -4,9 +4,23 @@
 // l'hippopotame perd 20 de poids lorsqu'il dort
 // ajouter une méthode swim() qui ne renvoie rien mais baisse le poids de 50.
 
+import Animal from './animal.class';
 import Food from './food/food';
 
-export default class Hippopotamus {
+export default class Hippopotamus extends Animal {
+  eat<T extends Food>(T: Food): void | string {
+    if (T.isVegan()) {
+      this.weight += 20;
+    } else {
+      return 'beurk';
+    }
+  }
 
-  // CODER ICI
+  sleep(): void {
+    this.weight = this.weight >= 20 ? this.weight - 20 : 0;
+  }
+
+  swim(): void {
+    this.weight = this.weight >= 50 ? this.weight - 50 : 0;
+  }
 }
