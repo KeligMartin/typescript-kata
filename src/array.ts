9159c2f4739d1c96ffe562a3fafbd5e05b4061d1
GabotThomas <gabotthomas@gmail.com>
2023-02-24 11:19:55 +0100

    Finish all test

--- src/array.ts
+++ src/array.ts
@@ -1,25 +1,24 @@
 export default class MyArray {
-
   // Coder une méthode oddNumbers qui prend en paramètre un tableau d'entier, et renvoie un tableau uniquement composé
   // des entiers impairs de ce dernier
   oddNumbers(arr: number[]) {
-    return undefined
+    return arr.filter((number) => number % 2 !== 0);
   }
 
   // Coder une méthode sort qui renvoie le tableau passé en paramètre trié par ordre croissant
   sort(arr: number[]) {
-    return undefined
+    return arr.sort((a, b) => a - b);
   }
 
   // Coder une méthode boringFunction qui renvoie true si les string du tableaux ont toutes la même longueur,
   // ou renvoie "relou ta fonction" sinon
   boringFunction(arr: string[]) {
-    return undefined
+    return arr.every((str) => str.length === arr[0].length) || 'relou ta fonction';
   }
 
   // Coder une méthode optionalArray qui renvoie true si les 2 arrays d'entiers passés en paramètres sont égaux, false sinon
   // Faire de même avec un 3e tableau passé en paramètre qui sera optionnel
-  optionalArray(a, b, c?) {
-    return undefined
+  optionalArray(a: number[], b: number[], c?: number[]) {
+    return a.toString() === b.toString() && (!c || a.toString() === c?.toString());
   }
 }
--- src/vehicle.ts
+++ src/vehicle.ts
@@ -1,12 +1,12 @@
 import { VehicleType } from './vehicletype.enum';
 
 export default class Vehicle {
-  type: VehicleType
-  name: string
+  type: VehicleType;
+  name: string;
 
   constructor(type: VehicleType, name: string) {
-    this.type = type
-    this.name = name
+    this.type = type;
+    this.name = name;
   }
 
   // Coder une méthode nois qui switchera sur le type du véhicule et renverra
@@ -15,19 +15,26 @@ export default class Vehicle {
   // brrrrrrrrrrrrr si le vehicule est un camion
   // skrrrt skrrrt dans tout autre cas
   noise() {
-    return undefined
+    switch (this.type) {
+      case VehicleType.Car:
+        return 'vrooooom';
+      case VehicleType.Moto:
+        return 'bep bep';
+      case VehicleType.Truck:
+        return 'brrrrrrrrrrrrr';
+      default:
+        return 'skrrrt skrrrt';
+    }
   }
 
   // Coder un callback qui renverra la methode noise précedemment codée
   // mais transformera la valeur de retour en majuscule
   myCb = () => {
-    return undefined
-  }
+    return this.noise().toUpperCase();
+  };
 
   // Coder une méthode accelerate qui prend en paramètre un callback et renvoie la valeur de noise() concaténée au callback
   accelerate(cb: () => string) {
-    return undefined
+    return this.noise().concat(cb());
   }
-
-
 }
