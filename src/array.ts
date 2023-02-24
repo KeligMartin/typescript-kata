1df43756d19b75f7d7b8d88e02f59499e52c6b5e
Fabien <86556152+BastosFab@users.noreply.github.com>
2023-02-24 11:44:07 +0100

    Fabien

--- src/array.ts
+++ src/array.ts
@@ -3,23 +3,26 @@ export default class MyArray {
   // Coder une méthode oddNumbers qui prend en paramètre un tableau d'entier, et renvoie un tableau uniquement composé
   // des entiers impairs de ce dernier
   oddNumbers(arr: number[]) {
-    return undefined
+    let oddNumbers = arr.filter((number) => number % 2 === 1)
+    return oddNumbers
   }
 
   // Coder une méthode sort qui renvoie le tableau passé en paramètre trié par ordre croissant
   sort(arr: number[]) {
-    return undefined
+    let sortedArray = arr.sort((a, b) => a - b)
+    return sortedArray
   }
 
   // Coder une méthode boringFunction qui renvoie true si les string du tableaux ont toutes la même longueur,
   // ou renvoie "relou ta fonction" sinon
   boringFunction(arr: string[]) {
-    return undefined
+    let sameLength = arr.every((string) => string.length === arr[0].length)
+    return sameLength ? true : "relou ta fonction"
   }
 
   // Coder une méthode optionalArray qui renvoie true si les 2 arrays d'entiers passés en paramètres sont égaux, false sinon
   // Faire de même avec un 3e tableau passé en paramètre qui sera optionnel
-  optionalArray(a, b, c?) {
-    return undefined
+  optionalArray(a: number[], b: number[], c?: number[]) {
+    return a.every((number, index) => number === b[index]) && (c ? a.every((number, index) => number === c[index]) : true)
   }
 }
