67149b289669316da0a7bd60e04138215f129826
Redak45 <43347242+Redak45@users.noreply.github.com>
2023-02-24 15:46:49 +0100

    Update animal.class.ts

--- src/animal.class.ts
+++ src/animal.class.ts
@@ -1,12 +1,37 @@
-import Food from './food/food';
-
 // Créer une class Animal qui implémente l'interface IAnimal
 // par défaut, la méthode eat augmente le poids de l'animal de 10
 // par défaut, la méthode sleep réduit le poids de l'animal de 10
 // le poids d'un animal ne peut pas être négatif
 
-export default class Animal {
+import Food from './food/food';
+import IAnimal from './animal.interface';
 
-  // CODER ICI
+export default class Animal implements IAnimal {
+  
+  name: string;
+  weight: number = 0;
+
+   constructor(name: string, weight: number){
+
+    this.weight = weight;
+    this.name = name;
+
+  }
+
+  eat(argument:Food): string | void {
+    this.weight += 10;
+  }
+
+  sleep(): void {
+    if(this.weight >= 10){
+
+      this.weight -= 10;
+    } else{
+      
+      this.weight = 0;
+    }
+
+  }
+
 
 }
