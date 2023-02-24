03a8d1a9a4e7e062cf0417f56fe466fceef78505
pierre_antona <antonapierre785@gmail.com>
2023-02-24 13:24:26 +0100

    Fix errors

--- src/array.ts
+++ src/array.ts
@@ -1,25 +1,36 @@
 export default class MyArray {
-
   // Coder une méthode oddNumbers qui prend en paramètre un tableau d'entier, et renvoie un tableau uniquement composé
   // des entiers impairs de ce dernier
-  oddNumbers(arr: number[]) {
-    return undefined
+  oddNumbers(arr: number[]): number[] {
+    const oddArr: number[] = [];
+    arr.forEach((num) => {
+      if (num % 2 != 0) oddArr.push(num);
+    });
+    return oddArr;
   }
 
   // Coder une méthode sort qui renvoie le tableau passé en paramètre trié par ordre croissant
-  sort(arr: number[]) {
-    return undefined
+  sort(arr: number[]): number[] {
+    return arr.sort((a, b) => a - b);
   }
 
   // Coder une méthode boringFunction qui renvoie true si les string du tableaux ont toutes la même longueur,
   // ou renvoie "relou ta fonction" sinon
-  boringFunction(arr: string[]) {
-    return undefined
+  boringFunction(arr: string[]): boolean | string {
+    let l: number = arr[0].length;
+    for (let i: number = 0; i < arr.length; i++) {
+      if (arr[i].length != l) return 'relou ta fonction';
+    }
+    return true;
   }
 
   // Coder une méthode optionalArray qui renvoie true si les 2 arrays d'entiers passés en paramètres sont égaux, false sinon
   // Faire de même avec un 3e tableau passé en paramètre qui sera optionnel
-  optionalArray(a, b, c?) {
-    return undefined
+  optionalArray(a: number[], b: number[], c?: number[]): boolean {
+    if (c) {
+      return JSON.stringify(a) == JSON.stringify(b) && JSON.stringify(a) == JSON.stringify(c);
+    } else {
+      return JSON.stringify(a) == JSON.stringify(b);
+    }
   }
 }
