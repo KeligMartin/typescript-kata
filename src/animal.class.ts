7b47bea626d642b4a4e489e36f4f8717d12729a2
Manon Lavenier <manon.lavenier@gmail.com>
2023-02-24 11:43:44 +0100

    Animal test

--- src/animal.class.ts
+++ src/animal.class.ts
@@ -1,12 +1,20 @@
 import Food from './food/food';
+import IAnimal from './animal.interface';
 
 // Créer une class Animal qui implémente l'interface IAnimal
 // par défaut, la méthode eat augmente le poids de l'animal de 10
 // par défaut, la méthode sleep réduit le poids de l'animal de 10
 // le poids d'un animal ne peut pas être négatif
 
-export default class Animal {
+export default class Animal implements IAnimal{
 
-  // CODER ICI
+  constructor(public name: string, public weight: number) {}
 
+  eat(food: Food) {
+    this.weight += 10;
+  }
+
+  sleep() {
+    this.weight -= 10;
+  }
 }
