55ab1f5314f39c63ac61a2606dd1db9dc09e652f
Redak45 <43347242+Redak45@users.noreply.github.com>
2023-02-24 15:48:00 +0100

    Update array.ts

--- src/array.ts
+++ src/array.ts
@@ -3,23 +3,56 @@ export default class MyArray {
   // Coder une méthode oddNumbers qui prend en paramètre un tableau d'entier, et renvoie un tableau uniquement composé
   // des entiers impairs de ce dernier
   oddNumbers(arr: number[]) {
-    return undefined
+
+    return arr.filter((n) => n % 2 !== 0);
+
   }
 
   // Coder une méthode sort qui renvoie le tableau passé en paramètre trié par ordre croissant
   sort(arr: number[]) {
-    return undefined
+
+    return arr.sort((a, b) => a - b);
+
   }
 
   // Coder une méthode boringFunction qui renvoie true si les string du tableaux ont toutes la même longueur,
   // ou renvoie "relou ta fonction" sinon
   boringFunction(arr: string[]) {
-    return undefined
+
+    var firstStringTable = arr[0].length;
+    var allStringsTable = arr.every((str) => str.length === firstStringTable);
+
+    return allStringsTable ? true : "relou ta fonction";
+    
   }
 
   // Coder une méthode optionalArray qui renvoie true si les 2 arrays d'entiers passés en paramètres sont égaux, false sinon
   // Faire de même avec un 3e tableau passé en paramètre qui sera optionnel
-  optionalArray(a, b, c?) {
-    return undefined
+  optionalArray(a:number[], b:number[], c?: number[]): boolean {
+
+    if (a.length !== b.length) {
+
+      return false;
+
+    }
+    if (c && a.length !== c.length) {
+
+      return false;
+
+    }
+    for (let i = 0; i < a.length; i++) {
+
+      if (a[i] !== b[i]) {
+        return false;
+
+      }
+      if (c && a[i] !== c[i]) {
+
+        return false;
+
+      }
+    }
+    return true;
+    
   }
 }
