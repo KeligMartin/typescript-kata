f59a26aa5945d4fbef45e2255104f4558e40ddb9
Kester KAKaça <kester.kaca@ecv-digital.com>
2023-02-24 17:55:16 +0100

    TP Typescript

--- src/animal.class.ts
+++ src/animal.class.ts
@@ -1,3 +1,4 @@
+import IAnimal from './animal.interface';
 import Food from './food/food';
 
 // Créer une class Animal qui implémente l'interface IAnimal
@@ -5,8 +6,21 @@ import Food from './food/food';
 // par défaut, la méthode sleep réduit le poids de l'animal de 10
 // le poids d'un animal ne peut pas être négatif
 
-export default class Animal {
+export default class Animal implements IAnimal {
+  weight: number = 80;
+  name: string;
 
-  // CODER ICI
+  constructor(name: string, weight: number) {
+    this.weight = weight;
+    this.name = name;
+  }
 
+  eat = (myFood: Food) => {
+      this.weight += 10;
+    }
+
+  sleep = () => {
+    this.weight -= 10;
+    if (this.weight < 0) this.weight = 0;
+  }
 }
--- src/array.ts
+++ src/array.ts
@@ -2,24 +2,42 @@ export default class MyArray {
 
   // Coder une méthode oddNumbers qui prend en paramètre un tableau d'entier, et renvoie un tableau uniquement composé
   // des entiers impairs de ce dernier
-  oddNumbers(arr: number[]) {
-    return undefined
+  oddNumbers(arr: number[]): number[] {
+    return arr.filter((key) => key % 2 !== 0
+    );
   }
 
   // Coder une méthode sort qui renvoie le tableau passé en paramètre trié par ordre croissant
-  sort(arr: number[]) {
-    return undefined
+  sort(arr: number[]): number[] {
+    return arr.sort((a, b) => a - b);
   }
 
   // Coder une méthode boringFunction qui renvoie true si les string du tableaux ont toutes la même longueur,
   // ou renvoie "relou ta fonction" sinon
-  boringFunction(arr: string[]) {
-    return undefined
+  boringFunction(arr: string[]): boolean | string {
+    for (let i = 1, l = arr.length; i < l; i++) {
+      if (arr[0].length != arr[i].length) {
+        return 'relou ta fonction';
+      }
+    }
+    return true;
   }
 
   // Coder une méthode optionalArray qui renvoie true si les 2 arrays d'entiers passés en paramètres sont égaux, false sinon
   // Faire de même avec un 3e tableau passé en paramètre qui sera optionnel
-  optionalArray(a, b, c?) {
-    return undefined
+  optionalArray(a: number[], b: number[], c?: number[]): boolean {
+    if (c) {
+      if (a.length !== b.length || a.length !== c.length) {
+        return false;
+      } else {
+        return (a.every((value: number, index) => value === b[index]) && a.every((value: number, index) => value === c[index]));
+      }
+    }
+
+    if (a.length !== b.length) {
+      return false;
+    } else {
+      return a.every((value: number, index) => value === b[index]);
+    }
   }
 }
